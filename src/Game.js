e94429810a2ed4da143849574be74545edabbadc
Anastasiya-Zhyrkevich <Anastasiya.Zhyrkevich@yandex.ru>
2022-06-13 23:35:48 +0300

    events

--- src/Game.js
+++ src/Game.js
@@ -7,7 +7,8 @@ import Field from './Field.js';
 import InputField from './InputField.js';
 
 
-const SAMPLES_CNT = 4;
+const WORD_LENGTH = 5;
+const BOXES_CNT = 4;
 const ATTEMPTS = 7;
 
 
@@ -15,19 +16,42 @@ export default class Game extends React.Component {
   constructor(props) {
     super(props);
     this.startNewGame = this.startNewGame.bind(this);
+    this.typingCurrentWord = this.typingCurrentWord.bind(this);
+    this.submitCurrentWord = this.submitCurrentWord.bind(this);
+
     this.state = {
       in_progress: false,
       words: [],
-      attempts: Array(ATTEMPTS).fill('')
+      attempts: Array(ATTEMPTS).fill(''),
+      current_word: '',
+      currrent_ind: 0
     };
   }
 
   startNewGame() {
-    let words = _.sample(allWords, SAMPLES_CNT);
+    let words = _.sample(allWords, BOXES_CNT);
     console.log(words);
     this.setState({
       in_progress: true,
       words
+    });
+  }
+
+  typingCurrentWord(word) {
+    console.log('typingCurrentWord');
+    this.setState({current_word: word});
+  }
+
+  submitCurrentWord(word) {
+    console.log('submitCurrentWord');
+
+    let attempts = this.state.attempts;
+    attempts[this.state.current_ind] = word;
+
+    this.setState({
+      attempts,
+      current_ind: this.state.current_ind + 1,
+      current_word: ''
     });
   }
 
@@ -40,7 +64,11 @@ export default class Game extends React.Component {
         <div>
           <div>Game has been started</div>
           <Field words={this.state.words} attempt_words={this.state.attempts}/>
-          <InputField spots_cnt={this.state.words[0].length}/>
+          <InputField
+            onTypingInputWord={this.typingCurrentWord}
+            onSubmitInputWord={this.submitCurrentWord}
+            slots_cnt={WORD_LENGTH}
+          />
         </div>
       );
     }
--- src/InputField.js
+++ src/InputField.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import LetterBox from './LetterBox.js';
-
-const ORDINARY = 'ordinary';
 
 
 export default class InputField extends React.Component {
+  // Invisible input field to catch entered words
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    if (event.target.value.length > this.props.slots_cnt) {
+      return;
+    }
+    this.setState({value: event.target.value});
+    this.props.onTypingInputWord(event.target.value);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.value.length !== this.props.slots_cnt) {
+      return;
+    }
+
+    this.props.onSubmitInputWord(this.state.value);
+    this.setState({value: ''});
+  }
+
+
   render() {
-
     return (
-      <div className="InputField">
-        {[...Array(this.props.spots_cnt)].map((_, ind) => <LetterBox key={ind} letter='' status={ORDINARY}/>)}
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+          autoFocus
+        />
+      </form>
     );
-
   }
 }
